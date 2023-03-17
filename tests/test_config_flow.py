e6effa8b1aad26e7e762b868a6c9f0b0baa8637d
Björn Dalfors <bjorn@binovi.se>
2023-03-17 13:18:18 +0100

    fix pre-commit errors

--- tests/test_config_flow.py
+++ tests/test_config_flow.py
@@ -22,7 +22,10 @@ from .const import MOCK_CONFIG
 @pytest.fixture(autouse=True)
 def bypass_setup_fixture():
     """Prevent setup."""
-    with patch("custom_components.saleryd_hrv.async_setup", return_value=True,), patch(
+    with patch(
+        "custom_components.saleryd_hrv.async_setup",
+        return_value=True,
+    ), patch(
         "custom_components.saleryd_hrv.async_setup_entry",
         return_value=True,
     ):
