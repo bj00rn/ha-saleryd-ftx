c6547703c604e08a9e9486ef2e7042f8154a464b
Björn Dalfors <bjorn@binovi.se>
2023-11-15 10:23:02 +0100

    chore(pipeline): add commitlint

---
+++ .github/commitlint.config.js
@@ -0,0 +1,13 @@
+const Configuration = {
+
+    extends: ['@commitlint/config-conventional'],
+
+    /*
+     * Any rules defined here will override rules from @commitlint/config-conventional
+     */
+    rules: {
+      'body-max-line-length': [2, 'always', 200],
+    },
+  };
+
+  module.exports = Configuration;
