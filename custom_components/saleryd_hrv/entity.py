d2bb70825d8cdc3c0bb1966a93204679abe50a7e
Björn Dalfors <bjorn@binovi.se>
2024-01-04 18:42:22 +0100

    chore: remove unused attribute

--- custom_components/saleryd_hrv/entity.py
+++ custom_components/saleryd_hrv/entity.py
@@ -16,11 +16,9 @@ class SaleryLokeVirtualEntity(Entity):
         entry_id,
         entity_description: EntityDescription,
     ) -> None:
-        self._id = entry_id
         self.entity_description = entity_description
         self._attr_name = entity_description.name
         self._attr_unique_id = f"{entry_id}_{slugify(entity_description.name)}"
-        self._id = entry_id
 
         self._attr_device_info = DeviceInfo(
             identifiers={(DOMAIN, entry_id)},
@@ -39,11 +37,9 @@ class SalerydLokeEntity(CoordinatorEntity):
         entity_description: EntityDescription,
     ) -> None:
         super().__init__(coordinator)
-        self._id = entry_id
         self.entity_description = entity_description
         self._attr_name = entity_description.name
         self._attr_unique_id = f"{entry_id}_{slugify(entity_description.name)}"
-        self._id = entry_id
 
         self._attr_device_info = DeviceInfo(
             identifiers={(DOMAIN, entry_id)},
